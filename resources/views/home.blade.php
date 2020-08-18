7267d924e8758d0fee6fc4e310c0c058b1b155d9
satkarlamsal <satkarlamsal0@gmail.com>
2020-08-18 18:00:58 +0545

    s

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -239,6 +239,15 @@
 
 <!------------------------------------CLIENTS END--------------->
 
+<!-- <maps -->
+<div class="container-fluid" style="height: 400px;">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3532.8774174336154!2d83.46041401456496!3d27.690183332853923!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3996868839e2a553%3A0x1194302b35728cbf!2sHUB%20IT%20TRAINING%20%26%20SOLUTION%20(Computer%20Institute)!5e0!3m2!1sen!2snp!4v1597749941627!5m2!1sen!2snp" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+</div>
+
+
+<!-- <maps -->
+
+
 
 
 
