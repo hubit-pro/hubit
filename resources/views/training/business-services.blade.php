c3be40f760f2a2c232f3c07d02fb5e04f52f9a8b
hubit-pro <hubitworkgroup@gmail.com>
2020-08-27 23:19:38 +0545

    edited business service page for training

--- resources/views/training/business-services.blade.php
+++ resources/views/training/business-services.blade.php
@@ -15,7 +15,7 @@
 <img src="img/business-services.jpg">
 <div class="container-fluid text-center header">
 <h1><strong>BUILDING TALENT FOR GROWTH & INNOVATION</strong></h1>
-<p>At Startup Institute, we’re training the modern workforce for the innovation economy.
+<p>At  Institute, we’re training the modern workforce for the innovation economy.
  Now more than ever, it’s critical to retain top talent and ensure you are providing employees
   with the tools that will propel your business forward.</p>
   </div>
@@ -26,8 +26,8 @@
     <h2 style="white-space: pre-wrap;"><strong>INJECT INNOVATION<br>INTO YOUR BUSINESS</strong></h2>
     <h3 style="white-space: pre-wrap;">Developing talent for high-growth and innovative companies is our core expertise:</h3>
      <ul data-rte-list="default">
-     <li><p >Startup Institute <a href="/hire">hiring partners</a> get unlimited
-      access to over 1,500 web development,digital marketing, web design, and sales alumni
+     <li><p >At Our Institute <a href="/hire">hiring partners</a> get unlimited
+      access to over 1,500 web development,digital marketing, web design,Mobile app,Hardware expert,Networking expert,sale alumni and many more I.T.  workforce
        who have gone through our programs and have come out ready to hit the ground running.<br>&nbsp;</p></li>
 
      <li><p >Our <a href="/training">corporate training services</a>&nbsp;are
@@ -42,6 +42,7 @@
 
     <div class="col-md-4 top-hire">
         <h3>Hire Top Talent</h3>
+        yo section lai alik best banau use placeholder instead of label
     <form action="#">
   <div class="row Names">
     <div class="col">
@@ -84,10 +85,9 @@
 <select class="form-control">
   <option>Select Services</option>
   <option>Hiring Talent</option>
-  <option>Part-time Training Programs</option>
   <option>Innovation Hakathons</option>
   <option>On-Site Training</option>
-  <option>Licensing Startup Institue Curriculum</option>
+  
 </select>
 </div>
 </div>
@@ -98,20 +98,92 @@
     </div>
 </div>
 
-<div class="container-fluid Michale">
-  <p>"Startup Institute is a hiring pipeline for us. Just like the big accounting / 
+<div class="container-fluid Michale">Satkar yeslai carousel banau
+  <p>"This Institute is a hiring pipeline for us. Just like the big accounting / 
     law firms and consultancies hang around Ivy league colleges to poach the best and 
-    brightest from the class, we partner with Startup Institute to tap into talent trained 
+    brightest from the class, we partner with HUB I.T. TRAINING to tap into talent trained 
     for the new economy."</p>
-<h6>— Michael Taylor, Co-founder and COO, Ladder.io</h6>
+<h6>— Lok Prasad Bhusal, CTO at Classictech Butwal</h6>
 </div>
 
 <div class="container-fluid Programs">
   <h2>PROGRAMS TO MEET YOUR ORGANIZATION'S NEEDS</h2>
+<div class="row mt-5">
+  <div class="col-md-3 " >
+  
+  
+  <div class="card-body">
+    <h5 class="card-title">Office Package</h5>
+    <p class="card-text">Fundamental of Computer S/w,H/w & N/w,Microsoft Office,Accounting in hard copy,Accounting Software,Photoshop.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  
+  <div class="col-md-3">
+  
+  
+  <div class="card-body">
+    <h5 class="card-title">Hardware Expert</h5>
+    <p class="card-text">Basic Hardware & Networking,Security System Installation and Repairing,Printer Repairing,Chip level Hardware Repairing</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+ 
+  <div class="col-md-3">
+    
+  
+  <div class="card-body">
+    <h5 class="card-title">Website Designer</h5>
+    <p class="card-text">Prototyping,Mockup Designing,UI/Ux,HTML,CSS,Javascript.Bootstrap,Jquery,Photoshop,Adobe XD,Git</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  
+  <div class="col-md-3">
+  
+ 
+  <div class="card-body">
+    <h5 class="card-title">Web developer</h5>
+    <p class="card-text">PHP,PHP frameworks,Javacript Frameworks,Asp.Net,Django,Ruby On Rails</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+
 <div class="row">
-  <div class="col-md-6">
-  <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="col-md-3">
+  
+  
+  <div class="card-body">
+    <h5 class="card-title">Mobile App Developer</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  
+  <div class="col-md-3">
+  
+  
+  <div class="card-body">
+    <h5 class="card-title">Graphics Designer</h5>
+    <p class="card-text">Adobe Photoshop,Indesign,Illustrator,XD,Light Room,Premeire</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  
+  <div class="col-md-3">
+    
+  
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  
+  <div class="col-md-3">
+  
+ 
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -119,44 +191,50 @@
   </div>
 </div>
   </div>
-  <div class="col-md-6">
-  <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+  <div class="row">
+    <div class="col-md-3">
+    
+    
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
   </div>
+    
+    <div class="col-md-3">
+    
+    
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+    
+    <div class="col-md-3">
+      
+    
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+    
+    <div class="col-md-3">
+    
+   
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+    </div>
+  
 </div>
-  </div>
 
-</div>
-<div class="row">
-  <div class="col-md-6">
-    <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-  </div>
-  <div class="col-md-6">
-  <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-  </div>
-
-</div>
-
-
-</div>
 
 <div class="container-fluid connect">
 <h1>READY TO GET STARTED?</h1>
