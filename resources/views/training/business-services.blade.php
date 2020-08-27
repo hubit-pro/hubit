ad0605fe740cb424a8a9acac93d2e706c03514f4
satkarlamsal <satkarlamsal0@gmail.com>
2020-08-27 18:57:02 +0545

    s

--- resources/views/training/business-services.blade.php
+++ resources/views/training/business-services.blade.php
@@ -13,7 +13,7 @@
 <div class="banner">
     
 <img src="img/business-services.jpg">
-<div class="container-fluid text-center">
+<div class="container-fluid text-center header">
 <h1><strong>BUILDING TALENT FOR GROWTH & INNOVATION</strong></h1>
 <p>At Startup Institute, we’re training the modern workforce for the innovation economy.
  Now more than ever, it’s critical to retain top talent and ensure you are providing employees
@@ -43,7 +43,7 @@
     <div class="col-md-4 top-hire">
         <h3>Hire Top Talent</h3>
     <form action="#">
-  <div class="row">
+  <div class="row Names">
     <div class="col">
       <label>First Name</label>
       <input type="text" class="form-control" placeholder="" required>
@@ -56,7 +56,7 @@
 </form>
 
 <form>
-  <div class="row">
+  <div class="row company">
     <div class="col">
     <label>Company</label>
       <input type="text" class="form-control" placeholder="" required>
@@ -68,10 +68,104 @@
   </div>
 </form>
 
+<form>
+  <div class="row E-mail">
+  <div class="col-md-12">
+<label>E-mail</label>
+<input class="form-control" type="text" placeholder="" required>
+</div>
+</div>
+</form>
 
+<form>
+  <div class="row Selection">
+  <div class="col-md-12">
+<label>Services You are Intrested in:</label>
+<select class="form-control">
+  <option>Select Services</option>
+  <option>Hiring Talent</option>
+  <option>Part-time Training Programs</option>
+  <option>Innovation Hakathons</option>
+  <option>On-Site Training</option>
+  <option>Licensing Startup Institue Curriculum</option>
+</select>
+</div>
+</div>
+</form>
+
+<button type="submit" class="btn btn-primary mb-2">Get Started</button>
      
     </div>
 </div>
+
+<div class="container-fluid Michale">
+  <p>"Startup Institute is a hiring pipeline for us. Just like the big accounting / 
+    law firms and consultancies hang around Ivy league colleges to poach the best and 
+    brightest from the class, we partner with Startup Institute to tap into talent trained 
+    for the new economy."</p>
+<h6>— Michael Taylor, Co-founder and COO, Ladder.io</h6>
+</div>
+
+<div class="container-fluid Programs">
+  <h2>PROGRAMS TO MEET YOUR ORGANIZATION'S NEEDS</h2>
+<div class="row">
+  <div class="col-md-6">
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+  <div class="col-md-6">
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+
+</div>
+<div class="row">
+  <div class="col-md-6">
+    <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+  <div class="col-md-6">
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+
+</div>
+
+
+</div>
+
+<div class="container-fluid connect">
+<h1>READY TO GET STARTED?</h1>
+<p>Uniquely positioned at the center of tech ecosystems, we've designed our programs
+   to open new doors for you.</p>
+   <a href="#" class="btn btn-primary">Go somewhere</a>
+
+</div>
+
 @include('includes/footer')
     
 </body>
