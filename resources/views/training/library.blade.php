5f59336565c0c9c1b5e082f958e6a3d4e2244037
satkarlamsal <satkarlamsal0@gmail.com>
2020-08-30 18:52:09 +0545

    s

--- resources/views/training/library.blade.php
+++ resources/views/training/library.blade.php
@@ -11,14 +11,385 @@
  
 <!-------------------------NAVBAR END--------------------------------->
 
-<div class="container-fluid">
-  
+
+
+
+
+
+
+
+
+
+<div class="container-fluid scroll m-0 p-0">
+<div class="row m-0" >
+<div class="col-md-2 m-0 p-0">
+
+<div id="sidebar">
+<a type="button" class="btn btn-primary" data-toggle="collapse" data-target="#demo">Select a courses</a>
+  <div id="demo" class="collapse"> 
+    
+  <a class="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+  <a class="active1" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a> 
 
 </div>
+  <a class="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+  <a class="active1" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a> <a class="active1" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a> <a class="active1" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a> <a class="active1" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a> <a class="active1" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a> <a class="active1" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a> <a class="active1" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a> <a class="active1" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a> <a class="active1" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a> <a class="active1" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a> <a class="active1" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a> <a class="active1" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a> <a class="active1" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a> <a class="active1" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a> <a class="active1" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a> <a class="active1" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a> <a class="active1" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a> <a class="active1" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a> <a class="active1" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
+</div>
 
+<div class="col-md-10">
+Search Results
+Web result with site links
 
+Bootstrap 3 Tutorial - W3Schoolswww.w3schools.com › bootstrap
+Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java ...
+You've visited this page 5 times. Last visit: 11/10/19
+BS Dropdowns
+Try it Yourself - BS Collapse - Dropdown divider - Dropup - ...
+Bootstrap Templates
+Tryit Editor v3.6 - John's Blog - Try it Yourself - ...
+BS Navbar
+Try it Yourself - Inverted Navbar - Navbar with dropdown - ...
+Bootstrap Get Started
+My First Bootstrap Page - BS Grid Basic - Try it Yourself - ...
+Bootstrap 4
+Bootstrap 4 Get Started - My First Bootstrap Page - Navbar - Buttons
+Bootstrap Tutorial
+Learn Bootstrap 3 ». Bootstrap is a free CSS framework. Bootstrap ...
+More results from w3schools.com »
+Web results
 
+Bootstrap 4 Get Started - W3Schoolswww.w3schools.com › bootstrap4 › bootstrap_get_start...
+Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java ...
 
+Bootstrap Examples - W3Schoolswww.w3schools.com › bootstrap4 › bootstrap_examples
+Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java ...
+People also ask
+What is bootstrap w3schools?
+
+What is the use of bootstrap?
+
+Should I learn bootstrap 3 or 4?
+
+What is bootstrap latest version?
+
+Feedback
+Web results
+
+Bootstrap 4 Templates - W3Schoolswww.w3schools.com › bootstrap4 › bootstrap_templates
+Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java ...
+
+Bootstrap 4 All CSS Classes - W3Schoolswww.w3schools.com › bootstrap4 › bootstrap_ref_all_...
+Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java ...
+
+Bootstrap 4 Containers - W3Schoolswww.w3schools.com › bootstrap4 › bootstrap_containers
+Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java ...
+
+Bootstrap Text/Typography - W3Schoolsw3schools-fa.ir › bootstrap › bootstrap_typography
+Bootstrap's Default Settings. Bootstrap's global default font-size is 14px, with a line-height of 1.428. This is applied to the <body> element and all paragraphs ...
+People also search for
+
+jQuery
+
+JavaScript
+
+HTML5
+
+Document Object Model
+
+XML
+
+PHP
+See more
+Feedback
+Searches related to w3schools bootstrap
+bootstrap cdn
+
+bootstrap 5
+
+bootstrap 3
+
+bootstrap 4 download
+
+bootstrap 4 datepicker w3schools
+
+bootstrap 4 footer w3schools
+
+bootstrap 4 components
+
+bootstrap 4 exercises
+
+Page navigation
+1	
+2
+3
+4
+5
+6
+7
+Next
+Footer links
+NepalButwal - From your Location History - Use precise location - Learn more
+HelpSend feedbackPrivacyTerms
+
+Search Results
+Web result with site links
+
+Bootstrap 3 Tutorial - W3Schoolswww.w3schools.com › bootstrap
+Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java ...
+You've visited this page 5 times. Last visit: 11/10/19
+BS Dropdowns
+Try it Yourself - BS Collapse - Dropdown divider - Dropup - ...
+Bootstrap Templates
+Tryit Editor v3.6 - John's Blog - Try it Yourself - ...
+BS Navbar
+Try it Yourself - Inverted Navbar - Navbar with dropdown - ...
+Bootstrap Get Started
+My First Bootstrap Page - BS Grid Basic - Try it Yourself - ...
+Bootstrap 4
+Bootstrap 4 Get Started - My First Bootstrap Page - Navbar - Buttons
+Bootstrap Tutorial
+Learn Bootstrap 3 ». Bootstrap is a free CSS framework. Bootstrap ...
+More results from w3schools.com »
+Web results
+
+Bootstrap 4 Get Started - W3Schoolswww.w3schools.com › bootstrap4 › bootstrap_get_start...
+Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java ...
+
+Bootstrap Examples - W3Schoolswww.w3schools.com › bootstrap4 › bootstrap_examples
+Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java ...
+People also ask
+What is bootstrap w3schools?
+
+What is the use of bootstrap?
+
+Should I learn bootstrap 3 or 4?
+
+What is bootstrap latest version?
+
+Feedback
+Web results
+
+Bootstrap 4 Templates - W3Schoolswww.w3schools.com › bootstrap4 › bootstrap_templates
+Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java ...
+
+Bootstrap 4 All CSS Classes - W3Schoolswww.w3schools.com › bootstrap4 › bootstrap_ref_all_...
+Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java ...
+
+Bootstrap 4 Containers - W3Schoolswww.w3schools.com › bootstrap4 › bootstrap_containers
+Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java ...
+
+Bootstrap Text/Typography - W3Schoolsw3schools-fa.ir › bootstrap › bootstrap_typography
+Bootstrap's Default Settings. Bootstrap's global default font-size is 14px, with a line-height of 1.428. This is applied to the <body> element and all paragraphs ...
+People also search for
+
+jQuery
+
+JavaScript
+
+HTML5
+
+Document Object Model
+
+XML
+
+PHP
+See more
+Feedback
+Searches related to w3schools bootstrap
+bootstrap cdn
+
+bootstrap 5
+
+bootstrap 3
+
+bootstrap 4 download
+
+bootstrap 4 datepicker w3schools
+
+bootstrap 4 footer w3schools
+
+bootstrap 4 components
+
+bootstrap 4 exercises
+
+Page navigation
+1	
+2
+3
+4
+5
+6
+7
+Next
+Footer links
+NepalButwal - From your Location History - Use precise location - Learn more
+HelpSend feedbackPrivacyTerms
+
+Search Results
+Web result with site links
+
+Bootstrap 3 Tutorial - W3Schoolswww.w3schools.com › bootstrap
+Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java ...
+You've visited this page 5 times. Last visit: 11/10/19
+BS Dropdowns
+Try it Yourself - BS Collapse - Dropdown divider - Dropup - ...
+Bootstrap Templates
+Tryit Editor v3.6 - John's Blog - Try it Yourself - ...
+BS Navbar
+Try it Yourself - Inverted Navbar - Navbar with dropdown - ...
+Bootstrap Get Started
+My First Bootstrap Page - BS Grid Basic - Try it Yourself - ...
+Bootstrap 4
+Bootstrap 4 Get Started - My First Bootstrap Page - Navbar - Buttons
+Bootstrap Tutorial
+Learn Bootstrap 3 ». Bootstrap is a free CSS framework. Bootstrap ...
+More results from w3schools.com »
+Web results
+
+Bootstrap 4 Get Started - W3Schoolswww.w3schools.com › bootstrap4 › bootstrap_get_start...
+Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java ...
+
+Bootstrap Examples - W3Schoolswww.w3schools.com › bootstrap4 › bootstrap_examples
+Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java ...
+People also ask
+What is bootstrap w3schools?
+
+What is the use of bootstrap?
+
+Should I learn bootstrap 3 or 4?
+
+What is bootstrap latest version?
+
+Feedback
+Web results
+
+Bootstrap 4 Templates - W3Schoolswww.w3schools.com › bootstrap4 › bootstrap_templates
+Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java ...
+
+Bootstrap 4 All CSS Classes - W3Schoolswww.w3schools.com › bootstrap4 › bootstrap_ref_all_...
+Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java ...
+
+Bootstrap 4 Containers - W3Schoolswww.w3schools.com › bootstrap4 › bootstrap_containers
+Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java ...
+
+Bootstrap Text/Typography - W3Schoolsw3schools-fa.ir › bootstrap › bootstrap_typography
+Bootstrap's Default Settings. Bootstrap's global default font-size is 14px, with a line-height of 1.428. This is applied to the <body> element and all paragraphs ...
+People also search for
+
+jQuery
+
+JavaScript
+
+HTML5
+
+Document Object Model
+
+XML
+
+PHP
+See more
+Feedback
+Searches related to w3schools bootstrap
+bootstrap cdn
+
+bootstrap 5
+
+bootstrap 3
+
+bootstrap 4 download
+
+bootstrap 4 datepicker w3schools
+
+bootstrap 4 footer w3schools
+
+bootstrap 4 components
+
+bootstrap 4 exercises
+
+Page navigation
+1	
+2
+3
+4
+5
+6
+7
+Next
+Footer links
+NepalButwal - From your Location History - Use precise location - Learn more
+HelpSend feedbackPrivacyTerms
+
+Search Results
+
+</div>
+</div>
+</div>
 
 
 
