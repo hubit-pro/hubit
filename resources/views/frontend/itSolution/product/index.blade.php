c2f8af402041790f1bd1a5ed39db3cbe94e3879f
satkarlamsal <satkarlamsal0@gmail.com>
2020-11-12 17:26:44 +0545

    s

--- resources/views/frontend/itSolution/product/index.blade.php
+++ resources/views/frontend/itSolution/product/index.blade.php
@@ -6,21 +6,43 @@
   <link rel="stylesheet" type="text/css" href="{{asset('css/solution/navsolution.css')}}">
   <link rel="stylesheet" type="text/css" href="{{asset('css/footer.css')}}">
   <link rel="stylesheet" type="text/css" href="{{asset('css/solution/products.css')}}">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+<script src="https://kit.fontawesome.com/ebfafc2eb8.js" crossorigin="anonymous"></script>
+
   
 
 @endpush
 @section('content')
 
 
+<section class="container-fluid mt-1 searchbar" >
+<div class="row">
+	<div class="col-md-2 title">
+		<h4><strong>HUBIT STORE</strong></h4>
+	</div>
+	<div class="col-md-10">
+<div class="searchbox animated zoomIn">
+                <form method="get">
+                    <input oninput="triggercross(this.value)" type="text" placeholder="search" id="search" name="search"
+                        class="search"><i class="fas fa-search"></i>
+                    <button onclick="close1()" id="closeid" type="reset" class="close">
+                        <i class="fas fa-times"></i>
+                        <p id="demo"></p>
+                    </button>
+                </form>
 
+			</div>
+			</div>
+			</div>
+</section>
 
 <!-- -----------------GALLERY START-------------->
-<section class="container-fluid">
+<section class="container-fluid mt-2">
   <div class="row">
 
 <!------------tabs start---------->
-    <div class="col-xl-2 tabs">
-      <div class="row click">
+    <div class="col-xl-2 tabs mt-3">
+      <!-- <div class="row click">
       	@if(isset($product_categories))
 	      	@foreach($product_categories as $key => $category)
 	        <div class="col-xl-2 p-2">
@@ -28,7 +50,25 @@
 	        </div>
 	        @endforeach
         @endif
-      </div>
+	  </div> -->
+	  
+
+<!-- Section: Categories -->
+
+<section class="container-fluid" style=" border-bottom: 1px solid rgba(128, 128, 128, 0.356);">
+<h5>Catagories</h5>
+
+  @if(isset($product_categories))
+	      	@foreach($product_categories as $key => $category)
+  <div class="text-muted small text-uppercase">
+<p class="mb-0"><a href="javascript:;" class="card-link-secondary" data-category-id="{{$category->id}}">{{$category->title}}</a></p>
+	@endforeach
+        @endif
+	
+
+<!-- Section: Categories -->
+
+</section>
     </div>
 <!------------tabs end----------> 
 
@@ -37,7 +77,7 @@
       <div class="row ">
 		@if(isset($products))
 		@foreach($products as $key => $product)
-	        <div class="col-xl-3 m-2  text-center p-1 gallen product product-{{$product->product_category_id}}">
+	        <div class="col-md-3 m-2  text-center p-1 gallen product product-{{$product->product_category_id}}">
 	        @foreach($product->images as $key => $image)
 	        	@if($key == 0)   					
 
@@ -53,18 +93,16 @@
 	<a href="#" class="product-item__name">{{$product->title}} </a>
 	
    <div class="product-item__description">    
-      <span>  {!! $product->description !!}	  </span>  
+      <span>  {!! $product->description !!}	</span>  
     
   </div>
-  
-    						<div class="product-item__price">
-    							<a href="#" class="btn btn--buy">BUY NOW</a>
-    							<div class="price">
-              <a href="#" class="delivery">Best price</a>
-                    <span  class="rouble">Rs<span>17000</span></span></div>
-
-    						</div>
-    					</div>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+  <div class="product-item__price">
+    <a href="#" class="btn btn--buy">BUY NOW</a>
+    <div class="price">
+    <a href="#" class="delivery">Best price</a>
+     <span  class="rouble">Rs<span>17000</span></span></div>
+    </div>
+ </div>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
 </div>
 
 
@@ -74,32 +112,7 @@
 	    @endif
 </div>
 </div>
-<!------------camera start---------->
-    <!-- <div class="col-xl-10 panel " >   
-      <div class="row ">
-		@if(isset($products))
-		@foreach($products as $key => $product)
-	        <div class="col-xl-3 m-2  text-center p-1 gallen product product-{{$product->product_category_id}}">
-	        @foreach($product->images as $key => $image)
-	        	@if($key == 0)
-		          <img src="{{asset('images/uploads/'. $image->image)}}" height="120px">
-		         @endif
-	         @endforeach
-	         <div class="ingallen">
-	             <p><ins>{{$product->title}}</ins></p>
-	             {!! $product->description !!}
-			 </div>
-			 
-			<button class="btn col">Buy Now</button>
-			<button class="btn col">Add to cart</button>
-			
-			 
-			</div>
-	    @endforeach
-	    @endif
-      </div>
-    </div>
- -->
+
 
   </div>
 </section>
@@ -149,5 +162,27 @@ function flip() {
 }
 
 // Image taken from 1924 - http://www.1924.us/post/110067977427
+</script>
+
+
+<script>
+	function triggercross(val) {
+    var val = document.getElementById("search");
+    var x = document.getElementById("closeid");
+    x.style.display = "block";
+}
+
+function close1() {
+
+    // document.getElementById("demo").innerHTML = "You wrote: " + val;
+    var x = document.getElementById("closeid");
+    if (x.style.display == "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+
 </script>
 @endpush
