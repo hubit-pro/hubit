e55e57098c11ecd4a9461fa9ae7da11decd8d275
satkarlamsal <satkarlamsal0@gmail.com>
2020-11-13 17:35:59 +0545

    s

--- resources/views/frontend/itSolution/product/index.blade.php
+++ resources/views/frontend/itSolution/product/index.blade.php
@@ -18,7 +18,7 @@
 <section class="container-fluid mt-1 searchbar" >
 <div class="row">
 	<div class="col-md-2 title">
-		<h4><strong>HUBIT STORE</strong></h4>
+		<h4><strong><a href="products">HUBIT STORE</a></strong></h4>
 	</div>
 	<div class="col-md-10">
 <div class="searchbox animated zoomIn">
@@ -41,7 +41,7 @@
   <div class="row">
 
 <!------------tabs start---------->
-    <div class="col-xl-2 tabs mt-3">
+    <div class="col-xl-2 tabs mt-3 m-0 p-0">
       <!-- <div class="row click">
       	@if(isset($product_categories))
 	      	@foreach($product_categories as $key => $category)
@@ -55,20 +55,91 @@
 
 <!-- Section: Categories -->
 
-<section class="container-fluid" style=" border-bottom: 1px solid rgba(128, 128, 128, 0.356);">
+<!-- <section class="container-fluid" style=" border-bottom: 1px solid rgba(128, 128, 128, 0.356);">
 <h5>Catagories</h5>
 
   @if(isset($product_categories))
 	      	@foreach($product_categories as $key => $category)
   <div class="text-muted small text-uppercase">
-<p class="mb-0"><a href="javascript:;" class="card-link-secondary" data-category-id="{{$category->id}}">{{$category->title}}</a></p>
+	          <a href="javascript:;" class="tab product-category card-link-secondary" data-category-id="{{$category->id}}" ><p>{{$category->title}}</p></a>
+
 	@endforeach
         @endif
-	
+	 -->
 
 <!-- Section: Categories -->
 
-</section>
+<!-- </section> -->
+
+
+
+ <div class="container-fluid">
+     <div class="row d-flex justify-content-center">
+         <div class="col p-0 m-0">
+             <div class="card m-0 p-0">
+                 <article class="filter-group">
+                     <header class="card-header"> <a href="#" data-toggle="collapse" data-target="#collapse_aside1" data-abc="true" aria-expanded="false" class="collapsed"> <i class="icon-control fa fa-chevron-down"></i>
+                             <h6 class="title">Categories </h6>
+                         </a> </header>
+                     <div class="filter-content collapse" id="collapse_aside1" style="">
+                         <div class="card-body">
+                             <ul class="list-menu">
+                             @if(isset($product_categories))
+	      	@foreach($product_categories as $key => $category)
+  <div class="text-muted small text-uppercase">
+	          <a href="javascript:;" class="tab product-category card-link-secondary" data-category-id="{{$category->id}}" ><p>{{$category->title}}</p></a>
+
+	@endforeach
+        @endif
+                             </ul>
+                         </div>
+                     </div>
+                 </article>
+                 <article class="filter-group">
+                     <header class="card-header"> <a href="#" data-toggle="collapse" data-target="#collapse_aside2" data-abc="true" aria-expanded="false" class="collapsed"> <i class="icon-control fa fa-chevron-down"></i>
+                             <h6 class="title">Price </h6>
+                         </a> </header>
+                     <div class="filter-content collapse" id="collapse_aside2" style="">
+                         <div class="card-body"> <input type="range" class="custom-range" min="0" max="100" name="">
+                             <div class="form-row">
+                                 <div class="form-group col-md-6"> <label>Min</label> <input class="form-control" placeholder="$0" type="number"> </div>
+                                 <div class="form-group text-right col-md-6"> <label>Max</label> <input class="form-control" placeholder="$1,0000" type="number"> </div>
+                             </div> <a href="#" class="highlight-button btn btn-medium button xs-margin-bottom-five" data-abc="true">Apply Now</a>
+                         </div>
+                     </div>
+                 </article>
+                 <article class="filter-group">
+                     <header class="card-header"> <a href="#" data-toggle="collapse" data-target="#collapse_aside3" data-abc="true" aria-expanded="false" class="collapsed"> <i class="icon-control fa fa-chevron-down"></i>
+                             <h6 class="title">Size </h6>
+                         </a> </header>
+                     <div class="filter-content collapse" id="collapse_aside3" style="">
+                         <div class="card-body"> <label class="checkbox-btn"> <input type="checkbox"> <span class="btn btn-light"> XS </span> </label> <label class="checkbox-btn"> <input type="checkbox"> <span class="btn btn-light"> SM </span> </label> <label class="checkbox-btn"> <input type="checkbox"> <span class="btn btn-light"> LG </span> </label> <label class="checkbox-btn"> <input type="checkbox"> <span class="btn btn-light"> XXL </span> </label> <label class="checkbox-btn"> <input type="checkbox"> <span class="btn btn-light"> XXXL </span> </label> </div>
+                     </div>
+                 </article>
+                 <article class="filter-group">
+                     <header class="card-header"> <a href="#" data-toggle="collapse" data-target="#collapse_aside4" data-abc="true" class="collapsed" aria-expanded="false"> <i class="icon-control fa fa-chevron-down"></i>
+                             <h6 class="title">Rating </h6>
+                         </a> </header>
+                     <div class="filter-content collapse" id="collapse_aside4" style="">
+                         <div class="card-body"> <label class="custom-control"> <input type="checkbox" checked="" class="custom-control-input">
+                                 <div class="custom-control-label">Better </div>
+                             </label> <label class="custom-control"> <input type="checkbox" checked="" class="custom-control-input">
+                                 <div class="custom-control-label">Best </div>
+                             </label> <label class="custom-control"> <input type="checkbox" checked="" class="custom-control-input">
+                                 <div class="custom-control-label">Good</div>
+                             </label> <label class="custom-control"> <input type="checkbox" checked="" class="custom-control-input">
+                                 <div class="custom-control-label">Not good</div>
+                             </label> </div>
+                     </div>
+                 </article>
+             </div>
+         </div>
+     </div>
+ </div>
+
+
+
+
     </div>
 <!------------tabs end----------> 
 
