3c1e291a7e8c66aaf9681be606a207bf3fd33700
webby-manish <57864586+webby-manish@users.noreply.github.com>
2021-02-14 10:38:30 +0545

    clear all the backend files but navheads are not reacheable

--- public/js/function.js
+++
@@ -1,1 +0,0 @@
-$(docoment).ready
--- resources/views/backend/dashboard/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('backend.common.layout.app')
-@section('title', 'Dashboard')
-
-@section('content-header')
-    @include('backend.common.content_header')
-@endsection
-@section('content')
-
-@endsection
